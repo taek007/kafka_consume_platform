bf62f6bc7a2deabed125a94cd752eb1350644c47
wangzhuang <wangzhuang@100tal.com>
2018-04-14 21:13:38 +0800

    测试

--- MyTest.java
+++ MyTest.java
@@ -1,15 +1,35 @@
 package com.test;
 
+//https://www.cnblogs.com/shaweng/p/4013320.html
+//https://blog.csdn.net/hudashi/article/details/7058982
 public class MyTest {
  public MyTest(){
   super();
  }
 
- public static int add(int a,int b) {
-  return a+b;
- }
+/*
+public static void main(String[] args){
+        System.out.println("Hello World!");
+		start("abc");
+}
+*/
+public static void add(int a,int b) {
+	//start("cc");
+	System.out.println( a+b);
+	return;
+}
 
- public boolean judge(boolean bool) {
+
+
+public boolean judge(boolean bool) {
    return !bool;
+}
+
+ public   static void test(int msg){
+	System.out.println(msg);
+	return;
  }
 }
+
+//MyTest test = new MyTest();
+//test.start("abc");
